5baf92f0fe0f66a79c9ea4519c213e489b7bbb0e
Patrick Laranjo <patrick.laranjo.s@gmail.com>
2023-02-20 16:19:51 +0100

    quick basic setup for msal

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,23 +1,46 @@
 // import './App.css'
+import react, { useState } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useMsal, useMsalAuthentication } from '@azure/msal-react';
+import { InteractionType } from '@azure/msal-browser';
 
 import Layout from './Layout';
 import Home from './Home';
 import Profile from './Profile'
 
 function App() {
+  useMsalAuthentication(InteractionType.Redirect);
+  const [m_strUser, setm_strUser] = useState("");
+
   const navigate = useNavigate();
 
-  return (
+  function Render() {
 
-    <Routes>
-      <Route path='/' element={<Layout />} >
-        <Route index element={<Home />} />
-        <Route path='/profile' element={<Profile />} />
-      </Route>
-    </Routes>
+    const { accounts } = useMsal();
 
-  )
+    try {
+      const username = accounts[0].username;
+      setm_strUser(username)
+    } catch (error) {
+
+    }
+  }
+
+  if (m_strUser != "") {
+    // console.log(m_strUser)
+    return (
+      <Routes>
+        <Route path='/' element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
+    )
+  } else {
+    return <>{Render()}<div>Please wait...</div></>
+  }
+
+
 }
 
 export default App
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { PublicClientApplication } from '@azure/msal-browser'
+import { MsalProvider } from '@azure/msal-react'
 
 import App from './App'
 import './index.css'
 
+const msalConfig = {
+  auth: {
+    clientId: "799e21ab-1e95-4e43-9b6f-4e4f772f69c5",
+    authority: "https://login.microsoftonline.com/564b7b99-6149-4bb9-8635-d7eb9f95bf11",
+    redirechtUri: "http://localhost:5173",
+  }
+}
+
+const msalInstance = new PublicClientApplication(msalConfig)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <MsalProvider instance={msalInstance}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </MsalProvider>
   </React.StrictMode>,
 )
